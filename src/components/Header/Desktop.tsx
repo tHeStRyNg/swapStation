06194fac20d5376fb0f326ac46c5f07b0e9ac1c3
tHeStRyNg <info@algorithmic.one>
2024-04-08 15:09:08 +0000

    refactored

--- src/components/Header/Desktop.tsx
+++ src/components/Header/Desktop.tsx
@@ -31,7 +31,7 @@ const Desktop: FC = () => {
             <div className="flex items-center justify-between gap-4 px-6">
               <div className="flex gap-4">
                 <div className="flex items-center w-6 mr-4">
-                  <Image src="https://app.sushi.com/images/logo.svg" alt="Sushi logo" width="24px" height="24px" />
+                  <Image src="https://app.swapstation.com/images/logo.svg" alt="SwapStation logo" width="24px" height="24px" />
                 </div>
                 {menu.map((node) => {
                   return <NavigationItem node={node} key={node.key} />
